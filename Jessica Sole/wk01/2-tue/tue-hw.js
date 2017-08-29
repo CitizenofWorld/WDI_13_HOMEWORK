a81f6c4e7802b7a94323b5a79500686e28d2ee2d
Jess <jesesnz@gmail.com>
2017-08-29 15:45:40 +1000

    homework tue-wk-1

---
+++ Jessica Sole/wk01/2-tue/tue-hw.js
@@ -0,0 +1,36 @@
+
+//Even Odd reporter
+
+// for (var i=0; i<=20; i++){
+// 	if(i % 2 === 0){
+// 		console.log( i + " is even");
+// 	}else{
+// 		console.log( i + " is odd");
+// 	}
+// }
+
+
+// Multiplication Tables
+
+// for (var i=0; i<=10; i++){
+// 	for (var j=0; j<=10; j++){
+// 		console.log(i * j);
+// 	}
+// }
+
+
+//Your Top Choices
+
+// favColors = ["red", "orange", "green", "blue"];
+
+// for (var i=0; i < favColors.length; i++){
+// 	if (favColors[i] === favColors[0]){
+// 	  console.log("My 1st color of choice is " + favColors[i]);
+// 	}else if(favColors[i] === favColors[1]) {
+// 	  console.log("My 2nd color choice is " + favColors[i]);
+// 	}else if(favColors[i] === favColors[2]){
+// 		console.log("My 3rd color of choice is " + favColors[i]);
+// 	}else{
+// 		console.log("My 4th color of choice is " + favColors[i]);
+// 	}
+// }
