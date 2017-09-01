cf2327c88df495d3238618c8b8e9d04186fb69c9
Jess <jesesnz@gmail.com>
2017-09-01 13:33:34 +1000

    Add prompt for user input

---
+++ Jessica Sole/wk01/5-fri/PT_Planner/main.js
@@ -0,0 +1,156 @@
+var alamein = ["flinders street", "richmond", "east richmond", "burnley", "hawthorne","glenferie"];
+var glenWaverly = ["flagstaff", "melbourne central", "parliament", "richmond", "kooyong", "tooronga"];
+var sandringham = ["southern cross", "richmond", "south yarra","prahan","windsor"];
+
+var originPosition =[] //array to reference origin position by pushing the given number to represent the chosen origin array and its origin index
+var destinationPos = [] //array to reference destination by pushing the given number to represent the chosen destination array and its  index
+
+
+var directions = function(){
+	var promptOrigin = prompt ("Enter your origin");
+ 	var promptDestination = prompt ("Enter your destination");
+	var origin = promptOrigin.toLowerCase();
+	var destination = promptDestination.toLowerCase();
+	var transfer = "richmond";
+	var originTransPos =[]; //array for transfer
+	var destinationTransPos = []; //array for transfer
+	var stops =0; // string iterated to keep track and log number of stops
+	var output =""; // string created to contain the output from directions function
+
+
+     //determines the array and position of origin
+		if (alamein.indexOf(origin) >= 0){
+		originPosition.push(1 , alamein.indexOf(origin));
+		originTransPos.push(1 , alamein.indexOf(transfer));
+	}else if (glenWaverly.indexOf(origin) >= 0){
+		originPosition.push(2 , glenWaverly.indexOf(origin));
+		originTransPos.push(2 , glenWaverly.indexOf(transfer));
+	}else if (sandringham.indexOf(origin) >= 0){
+		originPosition.push(3 , sandringham.indexOf(origin));
+		originTransPos.push(3 , sandringham.indexOf(transfer));
+	}
+
+        //determines the array and position of destination
+			if (alamein.indexOf(destination) >= 0){
+		destinationPos.push(1 , alamein.indexOf(destination));
+		destinationTransPos.push(1 , alamein.indexOf(transfer));
+	}else if (glenWaverly.indexOf(destination) >= 0){
+		destinationPos.push(2 , glenWaverly.indexOf(destination));
+		destinationTransPos.push(2 , glenWaverly.indexOf(transfer));
+	}else if (sandringham.indexOf(destination) >= 0){
+		destinationPos.push(3 , sandringham.indexOf(destination));
+		destinationTransPos.push(3 , sandringham.indexOf(transfer));
+	}
+
+	// if the origin and destination are on the same line and origin is before the destination this statement will run
+
+	 if (originPosition[0] === destinationPos[0] && originPosition[1] !== destinationPos[1] && originPosition[1] < destinationPos[1]){
+	 	//the for loop prints each stop in increasing order from origin to destination
+	 	for (var i=originPosition[1]; i < destinationPos[1]; i++ ){
+	 		if (originPosition[0] === 1){
+	 		    output += alamein[i];
+	 	    }else if (originPosition[0] === 2){
+	 		    output += glenWaverly[i];
+	 	    }else if (originPosition[0] === 3){
+	 		    output += sandringham[i];
+	 	    }
+	 	    output += " -----> "; // adds arrow to output in between each stop
+	 	    stops++; // adds number of stops to var stops
+	 	  }
+	 	}
+
+	// if the origin and destination are on the same line and origin is after the destination this statement will run
+
+	 if (originPosition[0] === destinationPos[0] && originPosition[1] !== destinationPos[1] && originPosition[1] > destinationPos[1]){
+	 	//the for loop prints each stop in decreasing order from origin to destination
+	 	for (var i=originPosition[1]; i > destinationPos[1]; i-- ){
+	 		if (originPosition[0] === 1){
+	 		    output += alamein[i];
+	 	    }else if (originPosition[0] === 2){
+	 		    output += glenWaverly[i];
+	 	    }else if (originPosition[0] === 3){
+	 		    output += sandringham[i];
+	 	    }
+	 	    output += " -----> "; // adds arrow to output in between each stop
+	 	    stops++; // adds number of stops to var stops
+	 	  }
+
+	 	}
+
+	 	// if the origin and destination are not on the same line and origin is before the transfer this statement will run
+
+	 if (originPosition[0] !== destinationPos[0] && originPosition[1] < originTransPos[1]){
+	 	//the for loop prints each stop in increasing order from origin to transfer
+	 	for (var i=originPosition[1]; i < originTransPos[1]; i++ ){
+	 		if (originPosition[0] === 1){
+	 		    output += alamein[i];
+	 	    }else if (originPosition[0] === 2){
+	 		    output += glenWaverly[i];
+	 	    }else if (originPosition[0] === 3){
+	 		    output += sandringham[i];
+	 	    }
+	 	    output += " -----> "; // adds arrow to output in between each stop
+	 	    stops++; // adds number of stops to var stops
+	 	  }
+
+	 	}
+
+// if the origin and destination are not on the same line and origin is after the transfer this statement will run
+	  if (originPosition[0] !== destinationPos[0] && originPosition[1] > originTransPos[1]){
+	 	//the for loop prints each stop in decreasing order from origin to transfer
+	 	for (var i=originPosition[1]; i > originTransPos[1]; i-- ){
+	 		if (originPosition[0] === 1){
+	 		    output += alamein[i];
+	 	    }else if (originPosition[0] === 2){
+	 		    output += glenWaverly[i];
+	 	    }else if (originPosition[0] === 3){
+	 		    output += sandringham[i];
+	 	    }
+	 	    output += " -----> "; // adds arrow to output in between each stop
+	 	    stops++; // adds number of stops to var stops
+	 	  }
+
+	 	}
+
+	 		 	// if the origin and destination are not on the same line and transfer is before the destination this statement will run
+
+	 if (originPosition[0] !== destinationPos[0] && destinationTransPos[1] < destinationPos[1]){
+	 	//the for loop prints each stop in increasing order from origin to transfer
+	 	for (var i=destinationTransPos[1]; i < destinationPos[1]; i++ ){
+	 		if (destinationTransPos[0] === 1){
+	 		    output += alamein[i];
+	 	    }else if (destinationTransPos[0] === 2){
+	 		    output += glenWaverly[i];
+	 	    }else if (destinationTransPos[0] === 3){
+	 		    output += sandringham[i];
+	 	    }
+	 	    output += " -----> "; // adds arrow to output in between each stop
+	 	    stops++; // adds number of stops to var stops
+	 	  }
+
+	 	}
+
+// if the origin and destination are not on the same line and the transfer is after the destination this statement will run
+
+	 if (originPosition[0] !== destinationPos[0] && destinationTransPos[1] > destinationPos[1]){
+	 	//the for loop prints each stop in increasing order from origin to transfer
+	 	for (var i=destinationTransPos[1]; i > destinationPos[1]; i-- ){
+	 		if (destinationTransPos[0] === 1){
+	 		    output += alamein[i];
+	 	    }else if (destinationTransPos[0] === 2){
+	 		    output += glenWaverly[i];
+	 	    }else if (destinationTransPos[0] === 3){
+	 		    output += sandringham[i];
+	 	    }
+	 	    output += " -----> "; // adds arrow to output in between each stop
+	 	    stops++; // adds number of stops to var stops
+	 	  }
+	 	}
+
+output += destination;
+console.log(output + "\n" + stops + " stops");
+
+}
+directions();
+
+
