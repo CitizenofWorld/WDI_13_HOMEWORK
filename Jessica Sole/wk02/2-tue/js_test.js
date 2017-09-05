183a9ecb12c13445d085843064fba3c9cf83b515
Jess <jesesnz@gmail.com>
2017-09-05 10:13:01 +1000

    week 1 assesment added

---
+++ Jessica Sole/wk02/2-tue/js_test.js
@@ -0,0 +1,41 @@
+//JS Variables
+
+var captain = "Jack";
+var phrase = "Oh " + captain + ", my " + captain ;
+
+//JS Conditionals
+
+var souls = 3;
+var lifeRafts = 2;
+
+if (souls > lifeRafts){
+	console.log("SOS!")
+}
+
+//Data Structures - JS Arrays
+
+var weekend = ["Saturday"];
+weekend.push("Sunday");
+weekend.push("Friday");
+weekend[0] = "day";
+weekend.pop("Friday");
+
+//Data Structures - JS Objects
+
+var brain = {
+	energylevel : 10
+}
+brain["dream"] = "electric sheep";
+
+// JS Functions
+var rectangleArea = 
+
+var addArea = function (){
+	
+}
+
+addArea(rectangleArea);
+console.log(rectangleArea);
+
+
+//  did not complete JS Functions. Have a feeling I am overthinking it.
