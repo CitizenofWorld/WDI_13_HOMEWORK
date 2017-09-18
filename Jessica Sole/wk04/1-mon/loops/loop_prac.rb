e5be707718973aa32dc425505bb1300ead46c888
Jess <jesesnz@gmail.com>
2017-09-19 06:03:07 +1000

    ruby loop homework

---
+++ Jessica Sole/wk04/1-mon/loops/loop_prac.rb
@@ -0,0 +1,26 @@
+#You are to generate a basic "guess my number" game. The computer will pick a random number between 0 and 10. The user will 
+#guess the number until they guess correctly
+require 'pry'
+
+
+# print 'Enter a Max number: '
+# x = gets.chomp.to_i
+user_guess = gets.chomp.to_i
+number = rand(1..10)
+
+puts 'Guess my number: '
+puts 'Wrong! Guess again: ' until user_guess == number
+puts 'You guessed it'
+
+
+# puts 'Guess my number: ' until user_guess == number
+# if user_guess < number
+# 	puts 'Guess Higher: '
+# else user_guess > number
+# 	puts 'Guess Lower: '
+# end
+
+
+
+binding.pry
+
