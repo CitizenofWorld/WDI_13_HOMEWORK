d47dcb208ffc7d61d49c32905b7494dcc2508bef
Jess <jesesnz@gmail.com>
2017-10-02 09:05:30 +1100

    movies 2 with some database &  css style

--- Jessica Sole/wk05/5-fri/sinatra/app.rb
+++ Jessica Sole/wk05/5-fri/sinatra/app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
 require 'pry'
+require_relative 'db_config'
+require_relative'models/movie'
+
 # always call the folder with index views
 #binding.pry
 
@@ -9,7 +12,9 @@ get '/' do
   erb :index
 end
 
-get '/answer' do
+
+# --- search for movie --
+get '/about' do
   @title = params[:title]
   @result = HTTParty.get("http://omdbapi.com/?s=#{@title}&apikey=#{
     ENV['OMDB_API_KEY']}")
@@ -19,4 +24,21 @@ get '/answer' do
  #    ENV['OMDB_API_KEY']}")
  #  @movie = @result
 	erb :search
+
 end
+
+
+#--- display a list of search results click for individual display
+get '/detail' do
+movie = params[:search]
+results = HTTParty.get("http://omdbapi.com/?t=#{movie}&apikey=#{
+    ENV['OMDB_API_KEY']}")
+@data = results.parsed_response
+erb :about
+
+end
+
+
+
+
+
---
+++ Jessica Sole/wk05/5-fri/sinatra/notes.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE movies_db;
+\c movies_db
+
+CREATE TABLE movies (
+id SERIAL4 PRIMARY KEY,
+poster_url TEXT,
+title VARCHAR(200),
+year INTEGER,
+released VARCHAR(200),
+runtime VARCHAR(200),
+genre VARCHAR(200),
+director VARCHAR(200),
+writer VARCHAR(200),
+actors VARCHAR(200),
+plot TEXT
+);
