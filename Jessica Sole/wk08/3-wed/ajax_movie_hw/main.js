f1ef40139053ed4980c4f4c7342fb528713a94e2
Jess <jesesnz@gmail.com>
2017-10-21 15:58:48 +1100

    omdb ajax

---
+++ Jessica Sole/wk08/3-wed/ajax_movie_hw/main.js
@@ -0,0 +1,34 @@
+// https://gist.github.com/epoch/c173a5b0c545babd3c40da93e150303a
+
+//console.log("working")
+
+$("#search-btn").on('click', function(event){
+	event.preventDefault();
+	var options = {
+		url:"http://www.omdbapi.com",
+		method: "get",
+		data: {
+			apikey:"2f6435d9",
+			s: $('input').val()
+		}
+	}
+
+	var appendMovie = function(response){
+		$('.results').addClass("show")
+		_.each(response.Search, function(movie){
+			// debugger
+			console.log(movie.Title)
+				var $h2 = $('<h2>').text( movie.Title );
+				var $year = $('<h3>').text( movie.Year);
+				var $img = $('<img/>', {
+					src: movie.Poster
+				});
+				$(".results").append( $h2 );
+				$(".results").append( $year );
+				$(".results").append( $img );
+		})
+	}
+
+	$.ajax( options ).done( appendMovie )
+
+});
